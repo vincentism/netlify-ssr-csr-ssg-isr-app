aeaee2e677abe47e2a9bbba8a0ee796d8b8a46d4
vincent <hkustliwenqian@hotmail.com>
2025-06-19 20:56:04 +0800

    Add files via upload

--- app/api/stockss/route.ts
+++ app/api/stockss/route.ts
@@ -26,7 +26,7 @@ import { revalidatePath } from 'next/cache'
 //     })(0);
 // });
 
-let count = 1;
+// let count = 1;
 export async function GET(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> },
@@ -36,18 +36,19 @@ export async function GET(
 
   // const response = await fetch('https://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=sz002095&scale=60&ma=no&datalen=2', {
   const response = await fetch('http://hq.sinajs.cn/list=sh600519', {
-    next: { revalidate: 1 },
+    next: { revalidate: 10 },
     headers: { Referer: 'https://finance.sina.com.cn/' },
   });
 
 
   // const data = await response.text() + JSON.stringify(dirsList);
-  const data = await response.text();
-  revalidatePath('/isr2')
+  const data = (await response.text()) + Math.random();
+  console.log('ddd', data);
+  // revalidatePath('/isr2')
 
   // const id = (await params).id;
   // e.g. Query a database for user with ID `id`
-  return new Response(JSON.stringify(data) + count++, {
+  return new Response(JSON.stringify(data), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });
